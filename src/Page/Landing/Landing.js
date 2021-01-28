1fc0663a0c4a7ca86c49dbdefa5f4ace1062432a
Saurav Panthee <saurav.panthee@gmail.com>
2021-01-28 16:37:46 -0600

    Added Navbar

---
+++ src/Page/Landing/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Lorem from '../../Components/LoremTest/Lorem'
+import Navbar from '../../Components/Navbar/Navbar'
+
+
+function Landing(props) {
+    return (
+        <section className="flex flex-col h-screen bg-blue-100 w-full">
+            <Navbar/>
+            
+        </section>
+    )
+}
+
+
+export default Landing
+
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		colors: {
+			transparent: 'transparent',
+      		current: 'currentColor',
+			white: colors.white,
+			red: colors.rose,
+			blue: colors.blue,
+			black: colors.black,
+			gray: colors.trueGray,
+      		indigo: colors.indigo,
+      		red: colors.rose,
+			yellow: colors.amber,
+			primarybg: "#041A32",
+			secondarybg:"#17366D",
+			darkpink:"#F44292",
+			darkblue:"#0B1238",
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
